0c63e995c9bee5d5b357d2b29ba5b07b505722ae
Margarita <m.novolokova@gmail.com>
2022-11-22 21:59:14 +0200

    Task 1

--- index.js
+++ index.js
@@ -4,3 +4,16 @@
 // 2) Замінити усі максимальні значення в масиві на значення 0
 
 
+
+// 1)
+const getAverage = function () {
+  if (arguments.length === 0) {
+    return null;
+  }
+  let resultSum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    resultSum += arguments[i];
+  }
+  return resultSum / arguments.length;
+};
+console.log(getAverage(10, 20, 30, 40, 50))
